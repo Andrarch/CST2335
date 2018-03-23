275c9833c76568a4474d2a6a679ac37fcca6b848
Andrew <andrew.archibald1@gmail.com>
2018-03-23 17:55:23 -0400

    Lab 6 mid

--- app/src/main/java/com/example/andrew/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/andrew/androidlabs/WeatherForecast.java
@@ -2,10 +2,12 @@ package com.example.andrew.androidlabs;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Xml;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
@@ -35,7 +38,7 @@ public class WeatherForecast extends Activity {
 
 
     public class ForecastQuery extends AsyncTask<String, Integer, String>{
-        String windSpeed,tempCurrent,tempMin,tempMax;
+        String windSpeed,tempCurrent,tempMin,tempMax,bitMap;
         Bitmap weatherImage;
 
         @Override
@@ -43,7 +46,7 @@ public class WeatherForecast extends Activity {
             //http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric
             HttpURLConnection conn;
             try {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric\n");
+                URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric");
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setReadTimeout(10000 /* milliseconds */);
                 conn.setConnectTimeout(15000 /* milliseconds */);
@@ -55,35 +58,49 @@ public class WeatherForecast extends Activity {
                 tempCurrent="Failed to connect";
                 return null;
             }
-            XmlPullParser parser = Xml.newPullParser();
-            try{
-
-
+            try {
+                XmlPullParser parser = Xml.newPullParser();
                 parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
                 parser.setInput(conn.getInputStream(), null);
-            }catch (Exception e){
-                tempMin="failure to construct parser";
-            }
-            try {
-                parser.require(XmlPullParser.START_TAG, null, "temperature");
-                tempCurrent = parser.getAttributeValue(null, "value");
-                publishProgress(20);
-                tempMin = parser.getAttributeValue(null, "min");
-                publishProgress(40);
-                tempMax = parser.getAttributeValue(null, "max");
-                publishProgress(60);
-            }catch(Exception e){
+                parser.nextTag();
+                tempCurrent=parser.getName();
+                while (parser.next() != XmlPullParser.END_DOCUMENT) {
+                    if (parser.getEventType() != XmlPullParser.START_TAG) {
+                        continue;
+                    }
+                    String name = parser.getName();
+                    // Starts by looking for the entry tag
+                    if (name.equalsIgnoreCase("temperature")) {
+
+                            tempCurrent = "Current: "+parser.getAttributeValue(null, "value");
+                            publishProgress(20);
+                            tempMin = "Min: "+parser.getAttributeValue(null, "min");
+                            publishProgress(40);
+                            tempMax = "Max: "+parser.getAttributeValue(null, "max");
+                            publishProgress(60);
+                    }
+
+                    if (name.equalsIgnoreCase("speed")){
+                            windSpeed="Speed: "+parser.getAttributeValue(null,"value");
+                    }
+
+                    if (name.equalsIgnoreCase("clouds")) {
+
+                        bitMap = "http://openweathermap.org/img/w/sunny.png";
+                        publishProgress(80);
+                    }
+
+                    parser.next();
+
+
+
+
+                    }
+
+                }catch(Exception e){
                     tempMax="failure to get temps";
             }
-            try{
-                parser.require(XmlPullParser.START_TAG,null,"speed");
-                windSpeed=parser.getAttributeValue(null,"value");
-                publishProgress(80);
-            }catch (Exception e){
-                windSpeed="failure to get wind speed";
-            }
-
-
+            setBitmap(bitMap);
             return null;
         }
 
@@ -109,6 +126,46 @@ public class WeatherForecast extends Activity {
             weatherProgress.setVisibility(View.INVISIBLE);
 
 
+        }
+    }
+    public void setBitmap(String url){
+        ImageView imageViewer=findViewById(R.id.weatherImage);
+        imageViewer.setImageBitmap(HttpUtils.getImage(url));
+
+
+    }
+
+    /**
+     * @author Terry E-mail: yaoxinghuo at 126 dot com
+     * @version create: 2010-10-21 ??01:40:03
+     */
+    static class HttpUtils {
+        public static Bitmap getImage(URL url) {
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    return BitmapFactory.decodeStream(connection.getInputStream());
+                } else
+                    return null;
+            } catch (Exception e) {
+                return null;
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }
+
+        public static Bitmap getImage(String urlString) {
+            try {
+                URL url = new URL(urlString);
+                return getImage(url);
+            } catch (MalformedURLException e) {
+                return null;
+            }
         }
     }
 
